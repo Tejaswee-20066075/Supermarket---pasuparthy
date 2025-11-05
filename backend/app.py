8dae60ff6dc9740a8e7cd8574abd1660d9f0bc27
Tejaswee-20066075 <Tejaswee.avulapati@gmail.com>
2025-11-05 21:59:42 +0000

    created flask and cors app.py
    added comments which can help me understand

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,9 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify      
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import hashlib
+
+app= Falsk (__name__) //creates flask app
+CORS(app)             //CORS means Cross origin request which joins frontend javascript to backend APIs
+
+
