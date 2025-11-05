99cfc243b512d8905816694135d260f2dcf109ec
Tejaswee-20066075 <Tejaswee.avulapati@gmail.com>
2025-11-05 22:10:51 +0000

    created database
    we have setup database. Stored in file database.bd

--- backend/app.py
+++ backend/app.py
@@ -6,4 +6,7 @@ import hashlib
 app= Falsk (__name__) //creates flask app
 CORS(app)             //CORS means Cross origin request which joins frontend javascript to backend APIs
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db' //here we are creating db which is sqlite
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False            
+db = SQLAlchemy(app)
 
