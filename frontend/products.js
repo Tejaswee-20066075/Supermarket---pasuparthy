b87a1b407ba0e14b8a70919cd53aa2a797b5a04d
Tejaswee-20066075 <Tejaswee.avulapati@gmail.com>
2025-11-30 00:47:21 +0000

    Update products.js
    when you want to edit a product by ID

--- frontend/products.js
+++ frontend/products.js
@@ -1,1 +1,3 @@
 var API_URL = "https://glowing-meme-jj7wq64jp4qjc4jg-5000.app.github.dev";  
+let editID = null;                                                                 //when you want to edit a product by ID
+
