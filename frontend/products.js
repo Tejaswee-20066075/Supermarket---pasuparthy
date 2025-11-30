f4921528679dee4b4fcbddefb1181b6375b2c7db
Tejaswee-20066075 <Tejaswee.avulapati@gmail.com>
2025-11-30 16:37:32 +0000

    updating correct API_URL

--- frontend/products.js
+++ frontend/products.js
@@ -1,4 +1,4 @@
-var API_URL = "https://glowing-meme-jj7wq64jp4qjc4jg-5000.app.github.dev";  
+var API_URL = window.location.origin;  
 let editID = null;                                                                 //when you want to edit a product by ID
 function loadProducts() {                                                          //function to grt all products
     fetch(API_URL + "/api/products")                                               //calls backend product api
