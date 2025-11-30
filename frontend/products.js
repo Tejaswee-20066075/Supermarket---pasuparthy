d8b3c7aa0848b12ce75efd8d6f7bf3903c0f6b81
Tejaswee-20066075 <Tejaswee.avulapati@gmail.com>
2025-11-30 00:51:32 +0000

    function to get all products

--- frontend/products.js
+++ frontend/products.js
@@ -1,3 +1,5 @@
 var API_URL = "https://glowing-meme-jj7wq64jp4qjc4jg-5000.app.github.dev";  
 let editID = null;                                                                 //when you want to edit a product by ID
-
+function loadProducts() {                                                          //function to grt all products
+    fetch(API_URL + "/api/products")                                               //calls backend product api
+  
